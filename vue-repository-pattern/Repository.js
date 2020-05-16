7dc669dc593e5eca9cf6a5dfa2e246e07e500715
Adam Conway <adam@conway.nz>
2020-05-16 20:18:26 +1200

    Update Repository.js

--- vue-repository-pattern/Repository.js
+++ vue-repository-pattern/Repository.js
@@ -5,8 +5,7 @@ const SERVER_URL = 'http://localhost:9499';
 // Create the axios object
 const repo = axios.create({
     baseURL: SERVER_URL,
-    timeout: 5000,
-    headers: {'token': localStorage.s_id}
+    timeout: 5000   
 });
 
 repo.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
@@ -14,7 +13,7 @@ repo.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
 // Add the auth header if it exists
 repo.interceptors.request.use(
     (config) => {
-        let token = localStorage.getItem('token');
+        let token = localStorage.getItem('s_id');
 
         if (token) {
             config.headers['X-Authorization'] = token;
