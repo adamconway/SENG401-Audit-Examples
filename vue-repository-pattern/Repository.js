d2781ab55ffd0eab6f46cde3760f0676572bee98
Adam Conway <adam@conway.nz>
2020-05-16 20:16:58 +1200

    Create Repository.js

---
+++ vue-repository-pattern/Repository.js
@@ -0,0 +1,31 @@
+import axios from "axios"
+
+const SERVER_URL = 'http://localhost:9499';
+
+// Create the axios object
+const repo = axios.create({
+    baseURL: SERVER_URL,
+    timeout: 5000,
+    headers: {'token': localStorage.s_id}
+});
+
+repo.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
+
+// Add the auth header if it exists
+repo.interceptors.request.use(
+    (config) => {
+        let token = localStorage.getItem('token');
+
+        if (token) {
+            config.headers['X-Authorization'] = token;
+        }
+
+        return config;
+    },
+
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+export default repo;
